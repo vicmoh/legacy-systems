fdcfb018b1d6909bc0f53d3ff57d2b182860b3a8
Vicky Mohammad <vimoh1213@gmail.com>
2018-12-15 08:32:03 -0500

    9:18

--- fortran.f90
+++
@@ -1,3 +0,0 @@
-program fortrantut
-
-end program fortrantut
---
+++ fortran/fortran.f90
@@ -0,0 +1,35 @@
+program fortrantut
+  ! force to declare all var
+  ! in fortran when defining a var
+  ! var is case insensitive
+  ! put it at the top
+  implicit none
+  character*20 :: name
+  character (len = 20) :: f_name, l_name
+  real, parameter :: PI = 3.1415
+  real :: r_num1 = 0.0, r_num2 = 0.0
+  double precision :: dbl_num = 1.123456789012345
+  integer :: i_num1 = 0, i_num2 = 0
+  logical :: can_vote = .true.
+  character (len = 10) :: month
+
+  ! to check the size of the type
+  print *, "Biggest Real ", huge(r_num1)
+  print *, "Biggest int ", huge(i_num1)
+  print *, "Smallest Real ", tiny(r_num1)
+  ! return number of bytes
+  print "(a4, i1)", "Int", kind(i_num1)
+  print "(a5, i1)", "Real", kind(i_num1)
+  print "(a7, i1)", "Double", kind(dbl_num)
+  print "(a8, i1)", "Logical", kind(can_vote)
+
+  ! printing in fortran
+  ! code here... 9:18 https://www.youtube.com/watch?v=__2UgFNYgf8
+
+  ! print 
+  print *, "What's your name "
+  read *, f_name, l_name
+  ! read input and store and then print
+  print *, "Hello, ", trim(f_name), " ", trim(l_name)
+
+end program fortrantut
