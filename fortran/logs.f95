6eadda037b0a9a0ec5dc865a9905e962f9c25259
Vicky Mohammad <vimoh1213@gmail.com>
2019-01-09 02:33:36 -0500

    [AUTO]

---
+++ fortran/logs.f95
@@ -0,0 +1,27 @@
+!@author: Vicky Mohammad
+!@email: mohammav@uoguelph.ca
+
+program jclark
+    
+
+!----------------------------------------------------
+! functions section
+!----------------------------------------------------
+
+contains
+    ! a function which reads the relevent log data 
+    ! required by calcLOGjclark() and calclLOGvolume()
+    ! from user
+    function getLOGdata()
+    end function getLOGdata
+
+    ! a function which calculates the volume of a log in
+    ! cubic metres, using the data obtained by getLOGdata()
+    function getLOGvolume()
+    end function getLOGvolume
+
+    ! a function which calculate the volume of a log in board
+    ! feet, using the data obtained by getLOGdata()
+    function calcLOGjclark()
+    end function calcLOGjclark
+end program
