6c330e82d41750060351524ba2ae2ffcf097a6be
Vicky Mohammad <vimoh1213@gmail.com>
2019-01-09 22:16:06 -0500

    Almost there 62

--- fortran/logs.f95
+++ fortran/logs.f95
@@ -25,7 +25,7 @@ contains
     ! from user
     subroutine getLOGdata(DS, DL, TL, KERF, V)
         implicit none
-        real :: DS, DL, TL, KERF, V
+        real, intent(inout) :: DS, DL, TL, KERF, V
         ! ask for log length
         print *, "LOG’S SCALING DIAMETER (INCHES): "
         read *, DS
@@ -59,59 +59,59 @@ contains
         real, intent(in) :: DS, DL, TL, KERF
         real, intent(inout) :: V
         ! other vars
-        real :: T, SL, D, XI, L
+        real :: T, SL, D, XI
         real :: XL, DEX, VADD, DC
-        integer :: a, b, c
-        ! for calc 
-
-        ! volum to start
+        integer :: a, b, c, L
+        
         V = 0.0
-
-        ! if log is less the 4 feet exit
-        if ( (TL-4.0) < 0) then
-            print *, "----------<<( Feedback )>>----------"
-            print *, "TOTAL LOG LENGTH IS LESS THAN FOUR FEET"
-            print *, "NO BOARD FOOT VOLUME WILL BE COMPUTED."
-            print *, "------------------------------------"
+        if (TL-4.0 < 0) then
+            print *, "Total log length is less than four feet."
+            print *, "No board foot volume will be computed."
             return
         end if
-        ! 2nd routine
+
+        ! 
         if (DL <= 0) then
             T = 0.5
         else
-            T = 4.0 * (DL - DS) / TL
-            
-            ! 3rd routine
-            do a = 1, 20
-                if (TL - (4.0 * a) < 0.0) exit
-                L = a - 1
-                SL = (4.0 * L)
-                ! 4rth routine
-                D = DS + (T / 4.0) * (TL - SL)
-                ! 5th routine
-                do b = 1, 4
-                    XI = (b)
-                    if (SL - TL + XI > 0) exit
-                end do
-                ! 8th routine
-                XL = XI - 1.0
-                DEX = DS + (T / 4.0) * (TL - SL - XL)
-                VADD = 0.055 * XL * DEX * DEX - 0.1775 * XL * DEX
-                ! 9th routine
-                do c = 1, L
-                    DC = D + T * (c - 1)
-                    V = V + 0.22 * DC * DC - 0.71 * DC
-                end do
-                V = V + VADD
-                ! 10th routine
-                if (KERF > 0) then
-                    return 
-                else
-                    V = 0.905 * V
-                    return
-                end if
-            end do
+            T = 4.0 * (DL-DS) / TL
         end if
+
+        ! find out how many full foot segment
+        do a = 1, 20
+            if (TL-(4*a) >= 0) then
+                continue
+            else 
+                exit
+            end if
+            L = a - 1
+            SL = (4 * L)
+            D = DS + (T/4.0) * (TL-SL)
+            ! find how mnay full feet
+            do b = 1, 4
+                XI = b
+                if ((SL-TL+XI) > 0 ) then 
+                    exit
+                end if
+            end do 
+            ! small end of logs 
+            XL = XI - 1.0
+            DEX = DS + (T/4.0) * (TL-SL-XL)
+            VADD = 0.055 * XL * DEX * DEX - 0.1775 * XL * DEX
+            do c = 1, L
+                DC = D + T * (c-1)
+                V = V + 0.22 * DC * DC - 0.71 * DC
+            end do
+            V=V+VADD
+            ! kerf
+            if (KERF > 0) then
+                V = 0.905 * V
+                return
+            else
+                return
+            end if
+        end do
+
         return
     end subroutine calcLOGjclark
 end program
