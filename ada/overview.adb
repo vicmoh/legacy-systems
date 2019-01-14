be28e3b35702c87e7480937b8f2d3e0c40bd0751
Vicky Mohammad <vimoh1213@gmail.com>
2019-01-13 21:56:34 -0500

    [AUTO]

--- ada/overview.adb
+++ ada/overview.adb
@@ -1,5 +1,24 @@
 with Ada.Text_IO; use Ada.Text_IO;
 procedure overview is
 begin
-    Put_Line("overview world!");
+    -- dec var
+    radius, circumference : float;
+    percentage : float := 0.0;
+    pi : constant float := 3.1415927;
+    a, b, c : character := '.';
+    h : constant string := "jedi";
+    falg : boolean := false;
+    success, failure : boolean;
+    -- constraints var
+    x : integer range 1..10;
+    
+    -- dec numbers
+    num1, num2, total : integer;
+    total := num1 + num2;
+
+    put("num1 + num2 = total");
+    put(total);
+    
+    put("overview world!");
+    
 end overview;
