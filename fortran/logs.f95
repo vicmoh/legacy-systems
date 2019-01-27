f57bc355845f1d4b981f016c77b47e93145ed2d6
Vicky Mohammad <vimoh1213@gmail.com>
2019-01-27 16:49:18 -0500

    [AUTO]

--- fortran/logs.f95
+++ fortran/logs.f95
@@ -55,7 +55,7 @@ program main
             tempDL = diameterInsideBark + (tempLength * 0.5)
         end if
 
-        ! calc the volume
+        ! calculate the volume
         radiusSmallEnd = (diameterInsideBark / 39.37) / 2.0
         radiusLargeEnd = (tempDL / 39.37) / 2.0
         logLength = totalLogLength / 3.2808
@@ -77,7 +77,8 @@ program main
         integer :: i
         real :: cubicResult
         
-        ! return if less that four feet
+        ! return if less that four feet.
+        ! if total logs length is less than 4 feet, no boards foot volume will be  computed
         if (totalLogLength-4.0 < 0) then
             print *, "Total log length is less than four feet."
             print *, "No board foot volume will be computed."
@@ -85,13 +86,15 @@ program main
         end if
 
         ! check if large end is taper
+        ! if the log's large end diameter is furnished to jclark a taper rate will be computed
+        ! if dl=0 the standard  assumption of half a inch per feet of log length will be used
         if (logsLargeEnds <= 0) then
             taperRate = 0.5
         else
             taperRate = 4.0 * (logsLargeEnds-diameterInsideBark) / totalLogLength
         end if
 
-        ! find out how many full foot segment
+        ! find out how many full four foot segment the log contains.
         do i = 1, 20
             if (totalLogLength-(4*i) >= 0) then
                 cycle
@@ -103,7 +106,8 @@ program main
         segLog = (4 * numLogs)
         diameterScale = diameterInsideBark + (taperRate/4.0) * (totalLogLength-segLog)
 
-        ! find how manay full feet
+        ! find how manay full feet of length are in the segment
+        ! less than four four feet long
         do i = 1, 4
             numOfFullFeet = i
             if ((segLog-totalLogLength+numOfFullFeet) > 0 ) then 
@@ -111,25 +115,31 @@ program main
             end if
         end do 
 
-        ! small end of logs 
+        ! moves the scaling diameter down to the end of the 4 foot segments
+        ! and increase according to the taper then calculate log volumes
+        ! at the small ends
         tempFullFeet = numOfFullFeet - 1.0
         DiaEnds = diameterInsideBark + (taperRate/4.0) * (totalLogLength-segLog-tempFullFeet)
         volumeAdded = 0.055 * tempFullFeet * DiaEnds * DiaEnds - 0.1775 * tempFullFeet * DiaEnds
+        
+        ! calculate volumeese in the portion  of the log containing whole
+        ! four foot segment
         do i = 1, int(numLogs)
             DiaCalc = diameterScale + taperRate * (i-1)
             boardFootVolume = boardFootVolume + 0.22 * DiaCalc * DiaCalc - 0.71 * DiaCalc
         end do
         boardFootVolume=boardFootVolume+volumeAdded
 
-        ! kerf
+        ! if kerf is greater than 0, international 1/8 inch volume is converted to 
+        ! the international 1/4 inch volume
         if (kerf > 0) then
             boardFootVolume = 0.905 * boardFootVolume
         end if
 
-        ! get the volume
+        ! get the volume cubic result
         cubicResult = getLOGvolume(diameterInsideBark, logsLargeEnds, totalLogLength, numLogs)
 
-        ! print volume result
+        ! print the volume and board feet
         print *, "-----<<( Output )>>-----"
         print *, "The board feet is ", boardFootVolume
         print *, "The volume is ", cubicResult
