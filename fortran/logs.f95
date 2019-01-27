2e92ba4bbc4335f5537a47551e3c916cc424736c
Vicky Mohammad <vimoh1213@gmail.com>
2019-01-27 15:35:43 -0500

    changed the name of the variables

--- fortran/logs.f95
+++ fortran/logs.f95
@@ -1,13 +1,14 @@
 !@author: Vicky Mohammad
 !@email: mohammav@uoguelph.ca
+!@description: A program that will calculate the log volume.
 
 program main
     ! create struct
     implicit none
-    real :: DS, DL, TL, KERF, V
+    real :: diameterInsideBark, logsLargeEnds, totalLogLength, kerf, boardFootVolume
     ! get logo and calculate
-    call getLOGdata(DS, DL, TL, KERF, V)
-    call calcLOGjclark(DS, DL, TL, KERF, V)
+    call getLOGdata(diameterInsideBark, logsLargeEnds, totalLogLength, kerf, boardFootVolume)
+    call calcLOGjclark(diameterInsideBark, logsLargeEnds, totalLogLength, kerf, boardFootVolume)
 
     !----------------------------------------------
     ! functions
@@ -18,118 +19,119 @@ program main
     ! a function which reads the relevent log data 
     ! required by calcLOGjclark() and calclLOGvolume()
     ! from user
-    subroutine getLOGdata(DS, DL, TL, KERF, V)
+    subroutine getLOGdata(diameterInsideBark, logsLargeEnds, totalLogLength, kerf, boardFootVolume)
         implicit none
-        real, intent(inout) :: DS, DL, TL, KERF, V
+        real, intent(inout) :: diameterInsideBark, logsLargeEnds, totalLogLength, kerf, boardFootVolume
         ! ask for log length
         print *, "LOG’S SCALING DIAMETER (INCHES): "
-        read *, DS
+        read *, diameterInsideBark
         print *, "DIB AT LOG’S LARGE END (INCHES) (0.0 IF 1/2 INCH TAPER): "
-        read *, DL
+        read *, logsLargeEnds
         print *, "TOTAL LOG LENGTH (FEET): "
-        read *, TL
+        read *, totalLogLength
         print *, "KERF: "
-        read *, KERF 
+        read *, kerf 
         ! assign to the struct
-        V = 0.0
+        boardFootVolume = 0.0
     end subroutine getLOGdata
 
     ! a function which calculates the volume of a log in
     ! cubic metres, using the data obtained by getLOGdata()
-    function getLOGvolume(DS, DL, TL, L) result(volume)
+    function getLOGvolume(diameterInsideBark, logsLargeEnds, totalLogLength, length) result(volume)
         implicit none
-        real, intent(inout) :: DS, DL, TL, L
-        real :: tempDL, tempL 
-        real :: volume, mds, mdl, mtl, a1, a2, PI
+        real, intent(in) :: diameterInsideBark, logsLargeEnds, totalLogLength, length
+        real :: tempDL, tempLength 
+        real :: volume, radiusSmallEnd, radiusLargeEnd, logLength
+        real :: areaSmallEnd, areaLargeEnd, PI
 
         ! assign values
-        tempL = L
-        tempDL = DL
+        tempLength = length
+        tempDL = logsLargeEnds
         PI = 3.14159265359
-        tempL = TL/4
+        tempLength = totalLogLength/4
 
-        ! get the DL and TL
+        ! get the logsLargeEnds and totalLogLength
         if (tempDL <= 0) then 
-            tempDL = TL + (tempL * 0.5)
+            tempDL = totalLogLength + (tempLength * 0.5)
         end if
 
         ! calc the volume
-        mds = (DS / 39.37) / 2.0
-        mdl = (tempDL / 39.37) / 2.0
-        mtl = TL / 3.2808
-        A1 = PI * mds * mds
-        A2 = PI * mdl * mdl
-        volume = ((a1 + a2) / 2) * mtl
+        radiusSmallEnd = (diameterInsideBark / 39.37) / 2.0
+        radiusLargeEnd = (tempDL / 39.37) / 2.0
+        logLength = totalLogLength / 3.2808
+        areaSmallEnd = PI * radiusSmallEnd * radiusSmallEnd
+        areaLargeEnd = PI * radiusLargeEnd * radiusLargeEnd
+        volume = ((areaSmallEnd + areaLargeEnd) / 2) * logLength
     end function getLOGvolume
 
     ! a function which calculate the volume of a log in board
     ! feet, using the data obtained by getLOGdata()
-    ! SUBROUTINE JCLARK (DS,DL,TL,KERF,V)
-    subroutine calcLOGjclark(DS, DL, TL, KERF, V)
+    ! SUBROUTINE JCLARK (diameterInsideBark,logsLargeEnds,totalLogLength,kerf,boardFootVolume)
+    subroutine calcLOGjclark(diameterInsideBark, logsLargeEnds, totalLogLength, kerf, boardFootVolume)
         ! assing param vars
         implicit none 
-        real, intent(inout) :: DS, DL, TL, KERF, V
+        real, intent(inout) :: diameterInsideBark, logsLargeEnds, totalLogLength, kerf, boardFootVolume
         ! other vars
         real :: T, SL, D, XI, L
         real :: XL, DEX, VADD, DC
-        integer :: a, b, c
+        integer :: i
         real :: V2
         
         ! return if less that four feet
-        if (TL-4.0 < 0) then
+        if (totalLogLength-4.0 < 0) then
             print *, "Total log length is less than four feet."
             print *, "No board foot volume will be computed."
             return
         end if
 
         ! check if large end is taper
-        if (DL <= 0) then
+        if (logsLargeEnds <= 0) then
             T = 0.5
         else
-            T = 4.0 * (DL-DS) / TL
+            T = 4.0 * (logsLargeEnds-diameterInsideBark) / totalLogLength
         end if
 
         ! find out how many full foot segment
-        do a = 1, 20
-            if (TL-(4*a) >= 0) then
+        do i = 1, 20
+            if (totalLogLength-(4*i) >= 0) then
                 cycle
             else 
                 exit
             end if
         end do
-        L = a - 1
+        L = i - 1
         SL = (4 * L)
-        D = DS + (T/4.0) * (TL-SL)
+        D = diameterInsideBark + (T/4.0) * (totalLogLength-SL)
 
-        ! find how mnay full feet
-        do b = 1, 4
-            XI = b
-            if ((SL-TL+XI) > 0 ) then 
+        ! find how manay full feet
+        do i = 1, 4
+            XI = i
+            if ((SL-totalLogLength+XI) > 0 ) then 
                 exit
             end if
         end do 
 
         ! small end of logs 
         XL = XI - 1.0
-        DEX = DS + (T/4.0) * (TL-SL-XL)
+        DEX = diameterInsideBark + (T/4.0) * (totalLogLength-SL-XL)
         VADD = 0.055 * XL * DEX * DEX - 0.1775 * XL * DEX
-        do c = 1, int(L)
-            DC = D + T * (c-1)
-            V = V + 0.22 * DC * DC - 0.71 * DC
+        do i = 1, int(L)
+            DC = D + T * (i-1)
+            boardFootVolume = boardFootVolume + 0.22 * DC * DC - 0.71 * DC
         end do
-        V=V+VADD
+        boardFootVolume=boardFootVolume+VADD
 
         ! kerf
-        if (KERF > 0) then
-            V = 0.905 * V
+        if (kerf > 0) then
+            boardFootVolume = 0.905 * boardFootVolume
         end if
 
         ! get the volume
-        V2 = getLOGvolume(DS, DL, TL, L)
+        V2 = getLOGvolume(diameterInsideBark, logsLargeEnds, totalLogLength, L)
 
         ! print volume result
         print *, "-----<<( Output )>>-----"
-        print *, "The board feet is ", V
+        print *, "The board feet is ", boardFootVolume
         print *, "The volume is ", V2
         print *, "------------------------"
 
