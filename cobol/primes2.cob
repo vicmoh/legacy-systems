b5ff0b7f2d33041b888d4611e8872a3e43dfa3b4
Vicky Mohammad <vimoh1213@gmail.com>
2019-03-23 19:38:51 -0400

    [AUTO]

--- cobol/primes2.cob
+++ cobol/primes2.cob
@@ -1,15 +1,15 @@
-    *>    header of the program
+       *> IDENTIFICATION DIVISION
        IDENTIFICATION DIVISION.
        PROGRAM-ID. tutorial1.
     
-    *>    environtment division
+       *> ENVIRONMENT DIVISION
        ENVIRONMENT DIVISION.
 
-    *>    configuration section
+       *> CONFIGURATION SECTION
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
 
-    *>    input output section
+       *> INPUT OUTPUT SECTION
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
        SELECT INPUT-FILE ASSIGN TO "./cobol/assets/primes.dat"
@@ -17,18 +17,22 @@
        SELECT OUTPUT-FILE ASSIGN TO "./cobol/assets/primes.out"
            ORGANIZATION IS LINE SEQUENTIAL.
 
-    *>    data division
+       *> DATA DIVISION
        DATA DIVISION.
        FILE SECTION.
-       FD INPUT-FILE.
-           01 INPUT-VAL PIC X(9999).
-       FD OUTPUT-FILE.
-           01 OUT-LINE PIC x(9999) VALUE SPACES.
-           
+         FD INPUT-FILE.
+             01 INPUT-VAL PIC X(9999).
+         FD OUTPUT-FILE.
+             01 OUT-LINE PIC x(9999) VALUE SPACES.
+       
+       *> WORKING STORAGE SECTION
        WORKING-STORAGE SECTION.
            77 N PICTURE S9(9).
            77 R PICTURE S9(9) USAGE IS COMPUTATIONAL.
            77 I PICTURE S9(9) USAGE IS COMPUTATIONAL.
+           77 TO-BE-PRINT PIC X(9).
+           77 WS-EOF PIC X.
+           77 LOOP PIC 9.
        01 IN-CARD.
            02 IN-N PICTURE 9(9).
            02 FILLER PICTURE X(71).
@@ -50,31 +54,60 @@
            02 FILLER PICTURE X VALUE SPACE.
            02 OUT-N PICTURE Z(8)9.
            02 FILLER PICTURE X(14) VALUE ' ILLEGAL INPUT'.
-
+       
        PROCEDURE DIVISION.
+           MOVE 1 TO LOOP.
            OPEN INPUT INPUT-FILE, OUTPUT OUTPUT-FILE.
            WRITE OUT-LINE FROM TITLE-LINE AFTER ADVANCING 0 LINES.
            WRITE OUT-LINE FROM UNDER-LINE AFTER ADVANCING 1 LINE.
        1. READ INPUT-FILE INTO IN-CARD AT END GO TO FINISH.
+           
+           *> MOVE IN-N TO N
            MOVE IN-N TO N.
            DISPLAY N.
-           IF N IS GREATER THAN 1 GO TO B1.
-           MOVE IN-N TO OUT-N.
-           WRITE OUT-LINE FROM ERROR-MESS AFTER ADVANCING 1 LINE.
-           GO TO 1.
-       B1. IF N IS LESS THAN 4 GO TO 3.
-           MOVE 2 TO R.
-       2. DIVIDE R INTO N GIVING I.
-           MULTIPLY R BY I.
-       IF I IS NOT EQUAL TO N GO TO B2.
-           MOVE IN-N TO OUT-N-2.
-           WRITE OUT-LINE FROM NOT-A-PRIME-LINE AFTER ADVANCING 1 LINE.
-       GO TO 1.
-           B2. ADD 1 TO R.
-       IF R IS LESS THAN N GO TO 2.
-       3. MOVE IN-N TO OUT-N-3.
+
+           *> WRITE ERROR MESSAGE
+           IF N < 2
+               MOVE IN-N TO OUT-N
+               WRITE OUT-LINE FROM ERROR-MESS AFTER ADVANCING 1 LINE
+               *> CONTINUE
+               GO TO 1
+           END-IF.
+
+           *> B1. IF N IS LESS THAN 4 GO TO 3
+           IF N > 3
+               MOVE 2 TO R
+
+               *> IF R IS LESS THAN N GO TO 2.    
+               PERFORM UNTIL LOOP < 1
+                   DIVIDE R INTO N GIVING I
+                   MULTIPLY R BY I
+                   
+                   *> GO TO B2
+                   IF I IS NOT EQUAL TO N
+                       ADD 1 TO R
+                   ELSE
+                       MOVE IN-N TO OUT-N-2
+                       WRITE OUT-LINE FROM NOT-A-PRIME-LINE AFTER ADVANCING 1 LINE
+                       *> CONTINUE
+                       GO TO 1
+                   END-IF
+
+                   *> IF R IS LESS THAN N GO TO 2.
+                   SUBTRACT 1 FROM N
+                   IF R > N
+                       MOVE 0 TO LOOP
+                   END-IF
+               END-PERFORM
+
+           END-IF. 
+            
+           *> THIS IS 3.
+           MOVE IN-N TO OUT-N-3.
            WRITE OUT-LINE FROM PRIME-LINE AFTER ADVANCING 1 LINE.
+           *> CONTINUE
            GO TO 1.
+           
        FINISH.
            CLOSE INPUT-FILE, OUTPUT-FILE.
            STOP RUN.
