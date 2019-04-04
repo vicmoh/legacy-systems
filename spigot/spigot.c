2a59fd233f741230b9bacaafbd23f8e2e8becb73
Vicky Mohammad <vimoh1213@gmail.com>
2019-04-04 16:07:11 -0400

    [AUTO]

--- spigot/spigot.c
+++ spigot/spigot.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv){
-	printf("Hello World!\n");
-	return 0;
+int main() {
+    int r[2800 + 1];
+    int i, k;
+    int b, d;
+    int c = 0;
+
+    for (i = 0; i < 2800; i++) {
+        r[i] = 2000;
+    }
+
+    for (k = 2800; k > 0; k -= 14) {
+        d = 0;
+
+        i = k;
+        for (;;) {
+            d += r[i] * 10000;
+            b = 2 * i - 1;
+
+            r[i] = d % b;
+            d /= b;
+            i--;
+            if (i == 0) break;
+            d *= i;
+        }
+        printf("%.4d", c + d / 10000);
+        c = d % 10000;
+    }
+
+    return 0;
 }
+
