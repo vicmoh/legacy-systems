16ebdab238f1b420c1b1788bc6fa05b0ed86a536
Vicky Mohammad <vimoh1213@gmail.com>
2019-04-04 23:40:54 -0400

    [AUTO]

--- spigot/spigot.c
+++ spigot/spigot.c
@@ -5,46 +5,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 /**
  * Spigot function to find PI.
  * @return malloc string of the resultt. Should be freed.
  */
-void spigot(int numOfDec){
-	// Declare setup																																																							
+char* spigot(int numOfDec){
+	char* toBeReturn = calloc(1024, sizeof(char));
+	// Declare varibles
 	int array[numOfDec + 1];
-	int i, k, b, d, c = 0;
-	
-	// Initalize the array
-	for(int x=0; x<numOfDec; x++){
-		array[x] = 2000;
-	}// End for
-	
-	// Loop through the and get find the PI
-	for(int x=numOfDec; x>0; x -= 14){
-		d = 0;
-		x = k;
-		while(true){
-			d += array[x] * 10000;
-            b = 2 * x - 1;
+    int i, k, b, d, c = 0;
 
-            array[x] = d % b;
+	// For loop and initialize the array
+    for (i = 0; i < numOfDec; i++) {
+        array[i] = 2000;
+    }// End for
+
+	// For loop
+    for (k = numOfDec; k > 0; k -= 14) {
+        d = 0;
+        i = k;
+        for (;;) {
+            d += array[i] * 10000;
+            b = 2 * i - 1;
+			// Calculate and find the remainder
+            array[i] = d % b;
             d /= b;
-            x--;
-            if (x == 0) break;
-            d *= x;
-		}// End while
-
-		// Print the spigot PI
-		printf("%.4d", c + d / 10000);
+            i--;
+            if (i == 0) break;
+            d *= i;
+        }//End while
+        sprintf(toBeReturn, "%s%.4d", toBeReturn, c + d / 10000);
+		toBeReturn = realloc(toBeReturn, sizeof(char) * (strlen(toBeReturn) + 8));
         c = d % 10000;
-	}// End for
+    }// End for loop
+	
+	return toBeReturn;
 }// End spigot function
+
+/**
+ * Write to an outout file
+ * @filename for the output
+ * @string to be outputed
+ */
+void write(char* filename, char* string){
+	FILE* file = fopen(filename,  "w+");
+	fprintf(file, "%s", string);
+	fclose(file);
+}// End file
 
 /**
  * Main function to run the code.
  */
 int main(int argc, char** argv){
-	spigot(2800);
+	// Get the output file name
+	char fileName[256] = {""};
+	printf("Enter the filename to output: \n");
+	fgets(fileName, 256, stdin);
+	// Write the output string to a file
+	char* outputString = spigot(1000);
+	printf("Writing...\n%s\n", outputString);
+	write(fileName, outputString);
+	free(outputString);
 	return 0;
 }// End main function 
